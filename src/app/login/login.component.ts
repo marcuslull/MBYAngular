96dcd1d5d239bb320448259d45450d1546c93540
Marcus Lull <marcuslull@gmail.com>
2024-03-06 15:38:47 -0800

    style for login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,13 +4,23 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/
 import {NgIf} from "@angular/common";
 import {Router} from "@angular/router";
 import {JwtAuthenticationService} from "../authentication/jwt-authentication.service";
+import {MatError, MatFormField, MatHint, MatInput, MatLabel} from "@angular/material/input";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     NgIf,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInput,
+    MatFormField,
+    MatIcon,
+    MatError,
+    MatLabel,
+    MatHint,
+    MatButton
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -29,14 +39,14 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      loginEmail: ['', [Validators.required, Validators.email]],
+      loginPassword: ['', Validators.required]
     });
   }
 
   onSubmit(): void {
     if (this.loginForm.valid) {
-      this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      this.loginService.login(this.loginForm.value.loginEmail, this.loginForm.value.loginPassword).subscribe({
         next: (body) => {
           this.jwtAuthenticationService.setJwtToken(body);
           this.router.navigate(["/home"])
