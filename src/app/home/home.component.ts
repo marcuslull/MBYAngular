eed63b83a96afae31c03107ca73425c8f31d8afc
Marcus Lull <marcuslull@gmail.com>
2024-03-07 12:56:55 -0800

    finishing sidenav styling and adding path to breadcrumbs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
 import {RegisterComponent} from "../register/register.component";
 import {YardsComponent} from "../yards/yards.component";
 import {MatIconModule} from "@angular/material/icon";
@@ -7,6 +7,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {LoginComponent} from "../login/login.component";
 import {MatDrawerContainer, MatSidenavModule} from "@angular/material/sidenav";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-home',
@@ -20,12 +21,21 @@ import {MatDrawerContainer, MatSidenavModule} from "@angular/material/sidenav";
     RegisterComponent,
     LoginComponent,
     MatDrawerContainer,
-    MatSidenavModule
+    MatSidenavModule,
+    NgIf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  protected breadcrumbText: string = "/ ";
+  ngOnInit(): void {
+      this.breadcrumbText = window.location.pathname;
+  }
+
+  topOfPage() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  }
 }
 
 
